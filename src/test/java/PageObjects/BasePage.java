4d7ff8ae45bfa2900ec43ef634fe229def673b3f
Aayush.Srikanth <aayush.srikanth@ten10.com>
2023-02-20 16:43:01 +0000

    Created Methods in BasePage: - sendGetRequest - getStatusCode - getBodyResponse

--- src/test/java/PageObjects/BasePage.java
+++ src/test/java/PageObjects/BasePage.java
@@ -1,4 +1,30 @@
 package PageObjects;
 
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 abstract class BasePage {
+
+    Response response;
+
+    public Response sendGetRequest(String url) {
+        return RestAssured.given()
+                .when()
+                .get(url)
+                .then()
+                .extract()
+                .response();
+
+    }
+
+    public int getStatusCode() {
+        return response.getStatusCode();
+    }
+
+    public String getBodyResponse(){
+        return response.getBody().asString();
+    }
+
 }
