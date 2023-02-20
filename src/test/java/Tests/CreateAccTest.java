232551a9f4662ea57b627e7d8e229e9ae2e82bc9
Aayush.Srikanth <aayush.srikanth@ten10.com>
2023-02-20 16:44:56 +0000

    Added two tests related to Home Page

--- src/test/java/Tests/CreateAccTest.java
+++ src/test/java/Tests/CreateAccTest.java
@@ -1,4 +1,26 @@
 package Tests;
 
+import PageObjects.HomePage;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CreateAccTest {
+
+    HomePage homePage = new HomePage();
+
+    @Test
+    public void Test1() {
+        Response response = RestAssured.get("http://3.11.77.136/index.php");
+        Assertions.assertEquals(200, response.getStatusCode());
+
+    }
+
+    @Test
+    public void Test2(){
+        homePage.getHomePage();
+        Assertions.assertEquals(200, homePage.getStatusCode());
+    }
+
 }
