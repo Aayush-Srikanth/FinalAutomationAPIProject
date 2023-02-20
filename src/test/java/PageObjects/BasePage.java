e641fd17acd3ae65ec71853fa1d8d67d98591393
SuzyLady <susanakinuli@gmail.com>
2023-02-20 15:00:16 +0000

    created methods to get status code and response

--- src/test/java/PageObjects/BasePage.java
+++ src/test/java/PageObjects/BasePage.java
@@ -4,19 +4,18 @@ import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.Test;
 
 abstract class BasePage {
 
-    public String getStatus(String requestParameter) {
+    public int getStatus(String requestParameter) {
         RequestSpecification httpRequest = RestAssured.given();
         Response response = httpRequest.request(Method.GET, requestParameter);
-        return "Status received => " + response.getStatusLine();
+        return response.getStatusCode();
     }
     public String getResponse(String requestParameter) {
         RequestSpecification httpRequest = RestAssured.given();
         Response response = httpRequest.request(Method.GET, requestParameter);
-        return "Response=>" + response.prettyPrint();
+        return response.prettyPrint();
     }
 
 }
