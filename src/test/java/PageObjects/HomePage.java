f8d6c5f1fc495ef7e991447266f3e508fbdccfba
Aayush.Srikanth <aayush.srikanth@ten10.com>
2023-02-20 16:44:00 +0000

    Added getHomePage method

--- src/test/java/PageObjects/HomePage.java
+++ src/test/java/PageObjects/HomePage.java
@@ -1,4 +1,10 @@
 package PageObjects;
 
-public class HomePage {
+public class HomePage extends BasePage{
+
+
+    public void getHomePage() {
+        String url = "http://3.11.77.136/index.php";
+        response = sendGetRequest(url);
+    }
 }
