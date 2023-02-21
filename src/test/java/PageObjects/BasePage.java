1956d70853457c147b7e7ddfb60185991b3d3ffd
Aayush.Srikanth <aayush.srikanth@ten10.com>
2023-02-21 12:31:42 +0000

    BasePage: - Added constructor to BasePage (Base URL/ httpRequest) - Cookie filter added
    CreateAccountPage:
    - Cookie filter added to retain SessionID and allow account creation test to pass

--- src/test/java/PageObjects/BasePage.java
+++ src/test/java/PageObjects/BasePage.java
@@ -1,8 +1,9 @@
 package PageObjects;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.cookie.CookieFilter;
 import io.restassured.http.ContentType;
-import io.restassured.http.Method;
+import io.restassured.http.Cookies;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
@@ -10,23 +11,38 @@ abstract class BasePage {
 
     Response response;
 
-    public String baseURL="http://3.11.77.136/index.php";
- 
+    RequestSpecification httpRequest;
+    private static final String BASE_URL = "http://3.11.77.136/index.php";
+
+    public BasePage() {
+        RestAssured.baseURI = BASE_URL;
+        httpRequest = RestAssured.given();
+
+    }
+
+    CookieFilter cookieFilter = new CookieFilter();
+
     public Response sendGetRequest(String url) {
-        return RestAssured.given()
+        response = RestAssured.given()
+                .filter(cookieFilter)
                 .when()
                 .get(url)
                 .then().contentType(ContentType.HTML)
                 .extract()
                 .response();
+        return response;
 
+    }
+
+    public Cookies getCookies() {
+        return response.getDetailedCookies();
     }
 
     public int getStatusCode() {
         return response.getStatusCode();
     }
 
-    public String getBodyResponse(){
+    public String getBodyResponse() {
         return response.getBody().asString();
     }
 
