9c3dfaf76f8ae0f44af57a9d73cf921b8821b23b
serine.betraoui <serine.betraoui@ten10.com>
2023-02-22 14:59:24 +0000

    Added selectProduct method in ShoppingPage, accepts two int parameters for product ID and quantity Adjusted addToCartUserSignedIn method, asserts 200 code and prints response body FOREVER

--- src/test/java/PageObjects/ShoppingPage.java
+++ src/test/java/PageObjects/ShoppingPage.java
@@ -1,4 +1,25 @@
 package PageObjects;
 
-public class ShoppingPage extends BasePage{
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShoppingPage extends BasePage {
+
+    public void selectProduct(int productId, int productQuantity) {
+        Map<String, Object> formData = new HashMap<>();
+        formData.put("token", "8934763adde45dbfa9445e979a178bff");
+        formData.put("id_product", productId);
+        formData.put("id_customization", "0");
+        formData.put("group[1]", "1");
+        formData.put("group[3]", "13");
+        formData.put("qty", productQuantity);
+        formData.put("add", "1");
+        formData.put("action", "update");
+
+
+        response = httpRequest.filter(cookieFilter)
+                .queryParam("controller", "cart")
+                .formParams(formData)
+                .post("/");
+    }
 }
