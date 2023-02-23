19b8de7490ffd6aa5d9dd1cfc1b17b0cde810d80
serine.betraoui <serine.betraoui@ten10.com>
2023-02-23 11:39:03 +0000

    addToCartTest is now functional - generated constructor in addToCartTest class - created retrieveToken method (hashmap, contains find token value gpath) in ProductPage class, accepts productId parameter like selectProduct method, and placed it in the selectProduct method instead of hardcoding the token - selectProduct method still accepts 2 values, id and quantity
    Small changes to the return in getGpathFromXMLBody method in BasePage class
    
    CartPage class added, contains getCartPage method and getProductLineInfo

--- src/test/java/PageObjects/ProductPage.java
+++ src/test/java/PageObjects/ProductPage.java
@@ -1,4 +1,35 @@
 package PageObjects;
 
-public class ProductPage extends BasePage{
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductPage extends BasePage {
+
+    public String retrieveToken(int productId) {
+        Map<String, Object> queryParams = new HashMap<>();
+        queryParams.put("id_product", productId);
+        queryParams.put("id_product_attribute", "1");
+        queryParams.put("rewrite", "faded-short-sleeves-tshirt");
+        queryParams.put("controller", "product");
+        sendGetRequest("", queryParams);
+        return getGpathFromXmlBody("**.find {it.@name == 'token'}.@value");
+    }
+
+    public void selectProduct(int productId, int productQuantity) {
+        Map<String, Object> formData = new HashMap<>();
+        formData.put("token", retrieveToken(productId));
+        formData.put("id_product", productId);
+        formData.put("id_customization", "0");
+        formData.put("group[1]", "1");
+        formData.put("group[3]", "13");
+        formData.put("qty", productQuantity);
+        formData.put("add", "1");
+        formData.put("action", "update");
+
+
+        response = httpRequest.filter(cookieFilter)
+                .queryParam("controller", "cart")
+                .formParams(formData)
+                .post("/");
+    }
 }
